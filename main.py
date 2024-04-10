0df74c0930e00b050885eb6970f5fe5b8143c868
IliaLadygin <IliaLadygin@users.noreply.github.com>
2024-04-10 18:33:18 +0700

    Initial point

---
+++ main.py
@@ -0,0 +1,22 @@
+from Lib.my_package.Event import EventFull, Event
+from Lib.my_package.EventModel import EventModel
+from Lib.my_package.EventPresenter import EventPresenter
+from Lib.my_package.EventView import EventView
+
+# Создаем несколько заметок
+# Добавить чтение событий из файла
+events = [EventFull("Event 1", "20230404", "20230604", note="This is just a note"), EventFull("Event 2", "20230405", "20230407")]
+# Создаем модель заметок
+event_model = EventModel(events)
+# Создаем Presenter для заметок и связываем его с View и Model
+event_presenter = EventPresenter(EventView(EventPresenter), event_model)
+print(event_presenter.model.get_events())
+# Добавляем новую заметку
+event_presenter.add_event(EventFull("Event 3", "20230204", "20230604"))
+print(*event_presenter.model.get_events(), sep="\n")
+# Удаляем заметку
+event_presenter.delete_event_by_index(0)
+print(event_presenter.model.get_events())
+# Выводим все события на экран
+print(event_presenter.model.get_events())
+print(5)
